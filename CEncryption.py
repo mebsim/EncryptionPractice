d24af77ff17198dae81fdb25358a4d08614724ac
mebsim <mebsim@users.noreply.github.com>
2017-03-30 13:04:49 -0400

    Create CEncryption.py

---
+++ CEncryption.py
@@ -0,0 +1,12 @@
+letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+original = input("What is the sentence? ")
+original = original.upper()
+originalList = list(original)
+rotations = int(input("How many rotations? "))
+for i in range(0,len(original)):
+    for j in range(0,len(letters)):
+        if originalList[i] == letters[j]:
+            originalList[i] = letters[(j+rotations)%26]
+            break
+for i in range(0,len(originalList)):
+    print(originalList[i], end="")
